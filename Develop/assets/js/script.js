43881d46074780019f61908167b075ddc746a60e
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2022-05-13 13:01:10 -0400

    save to local

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -2,5 +2,19 @@ var currentDay = $('#currentDay');
 var today = new Date();
 currentDay.text(today.toLocaleDateString());
 
+// current hour adds bootstrap class to element
 var hour = today.getHours();
 $('#_' + hour).addClass('present');
+
+// event listeners for save button
+$('button').each((i, button) => {
+    $(button).click(saveEvent);
+});
+
+function saveEvent() {
+    var row = $(this).closest('.row');
+    var hour = row.attr('id');
+    var event = row.find('textarea').val();
+
+    localStorage.setItem(hour, event);
+}
